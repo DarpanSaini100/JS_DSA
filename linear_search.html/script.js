83f0ba8a657e547e45e30b3e10cc7dc36f369ae6
DarpanSaini100 <darpansaini19978@gmail.com>
2024-10-25 13:56:57 +0530

    complete linera search

--- linear_search.html/script.js
+++ linear_search.html/script.js
@@ -1,16 +1,21 @@
-let data = [30, 97, 87, 56, 34, 22, 4];
-let find = 22;
-let found = false;
 
-for (let i = 0; i < data.length; i++) {
-    if (data[i] === find) {
-        console.log(i);
-        found = true; // Item is found
-        break; // Exit the loop
+function searchEl(){
+    let data = [30, 97, 87, 56, 34, 22, 4];
+     let find = document.getElementById("el").value;
+     find= parseInt(find);
+     let found = false;
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] === find) {
+            console.log(i);
+            found = true; // Item is found
+            break; // Exit the loop
+        }
+    }
+    
+    // After the loop, use if-else to check if found
+    if (!found) {
+        console.log("item not found");
     }
 }
 
-// After the loop, use if-else to check if found
-if (!found) {
-    console.log("item not found");
-}
